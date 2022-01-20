1122b99af1a642c096e9763c2d638dc7a76b3cec
Hu Xulm <huxulm@gmail.com>
2022-01-20 19:09:11 +0800

    update

--- 0015.3sum/solution.go
+++ 0015.3sum/solution.go
@@ -2,9 +2,9 @@ package sum
 
 import "sort"
 
-func twoSumTarget(nums []int, target int) [][]int {
+func twoSumTarget(nums []int, start, target int) [][]int {
 	sort.Ints(nums)
-	lo, hi := 0, len(nums)-1
+	lo, hi := start, len(nums)-1
 	ans := make([][]int, 0)
 	for lo < hi {
 		left, right := nums[lo], nums[hi]
@@ -31,6 +31,57 @@ func threeSum(nums []int) [][]int {
 	sort.Ints(nums)
 
 	ans := make([][]int, 0)
+	n := len(nums)
 
+	// 穷举threeSum第一个数字
+	for i := 0; i < n; i++ {
+		tuples := twoSumTarget(nums, i+1, -nums[i])
+		for _, tuple := range tuples {
+			tuple = append([]int{nums[i]}, tuple...)
+			ans = append(ans, tuple)
+		}
+		// 移除第一个数字重复的情况
+		for ; i < n-1 && nums[i] == nums[i+1]; i++ {
+		}
+	}
+
+	return ans
+}
+
+// LC O(n^2)
+func threeSum1(nums []int) [][]int {
+	n := len(nums)
+	sort.Ints(nums)
+	ans := make([][]int, 0)
+
+	// 枚举a
+	for first := 0; first < n; first++ {
+		// 需要和上一次枚举的数不相同
+		if first > 0 && nums[first] == nums[first-1] {
+			continue
+		}
+		// c 对应的指针初始指向数组的最右端
+		third := n - 1
+		target := -1 * nums[first]
+		// 枚举b
+		for second := first + 1; second < n; second++ {
+			// 需要和上一次枚举的数不相同
+			if second > 1 && nums[second] == nums[second-1] {
+				continue
+			}
+			// 需要保证 b 的指针在 c 的指针的左侧
+			for second < third && nums[second]+nums[third] > target {
+				third--
+			}
+			// 如果指针重合，随着 b 后续的增加
+			// 就不会有满足 a+b+c=0 并且 b<c 的 c 了，可以退出循环
+			if second == third {
+				break
+			}
+			if nums[second]+nums[third] == target {
+				ans = append(ans, []int{nums[first], nums[second], nums[third]})
+			}
+		}
+	}
 	return ans
 }
--- 0015.3sum/solution_test.go
+++ 0015.3sum/solution_test.go
@@ -9,7 +9,14 @@ func TestTwoSumTarget(t *testing.T) {
 	input := []int{1, 3, 1, 2, 2, 3}
 	expect := [][]int{{1, 3}, {2, 2}}
 	target := 4
-	if r := twoSumTarget(input, target); !reflect.DeepEqual(r, expect) {
+	if r := twoSumTarget(input, 0, target); !reflect.DeepEqual(r, expect) {
+		t.Errorf("Expect: %v but got: %v", expect, r)
+	}
+}
+func TestThreeSumTarget(t *testing.T) {
+	input := []int{-1, 0, 1, 2, -1, -4}
+	expect := [][]int{{-1, -1, 2}, {-1, 0, 1}}
+	if r := threeSum(input); !reflect.DeepEqual(r, expect) {
 		t.Errorf("Expect: %v but got: %v", expect, r)
 	}
 }
