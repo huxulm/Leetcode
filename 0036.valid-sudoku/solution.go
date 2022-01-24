2970ccada7cd0667d14683bc3c9c1d06d1aeec54
Hu Xulm <huxulm@gmail.com>
2022-01-24 17:42:28 +0800

    update

--- 0036.valid-sudoku/solution.go
+++ 0036.valid-sudoku/solution.go
@@ -77,3 +77,24 @@ func backtrack(board [][]byte, n int, marked [][]bool, visRow, visCol []bool, i,
 
 	return backtrack(board, n, marked, visRow, visCol, i+1, j) && backtrack(board, n, marked, visRow, visCol, i, j+1)
 }
+
+// official
+func isValidSudoku1(board [][]byte) bool {
+	var rows, columns [9][9]int
+	var subboxes [3][3][9]int
+	for i, row := range board {
+		for j, c := range row {
+			if c == '.' {
+				continue
+			}
+			index := c - '1'
+			rows[i][index]++
+			columns[j][index]++
+			subboxes[i/3][j/3][index]++
+			if rows[i][index] > 1 || columns[j][index] > 1 || subboxes[i/3][j/3][index] > 1 {
+				return false
+			}
+		}
+	}
+	return true
+}
