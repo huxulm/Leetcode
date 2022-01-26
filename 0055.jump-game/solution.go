f075b2ad963fed21063a5382e5829e3a4261de16
Hu Xulm <huxulm@gmail.com>
2022-01-27 00:11:23 +0800

    update

--- 0055.jump-game/solution.go
+++ 0055.jump-game/solution.go
@@ -5,7 +5,7 @@ func canJump(nums []int) bool {
 	n := len(nums)
 	for i := 0; i < n; i++ {
 		// 无法到达i+1
-		if nums[i] == 0 && maxPos <= i {
+		if maxPos < i {
 			break
 		}
 		maxPos = max(maxPos, i+nums[i])
