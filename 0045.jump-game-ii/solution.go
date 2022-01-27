55601aa84a91e68897ffe6ba225b7583efc5f7fb
Hu Xulm <huxulm@gmail.com>
2022-01-27 19:48:23 +0800

    update

--- 0045.jump-game-ii/solution.go
+++ 0045.jump-game-ii/solution.go
@@ -23,3 +23,25 @@ func max(a, b int) int {
 	}
 	return b
 }
+
+func jump1(nums []int) int {
+	n := len(nums)
+	dp := make([]int, n)
+	dp[0] = 0
+	maxPos := 0
+
+	for i := 1; i < n; i++ {
+		// 设定总是可以到达最后
+		// if i > maxPos {
+		// 	break
+		// }
+		if i+nums[i] <= maxPos {
+			dp[i] = dp[i-1]
+		} else {
+			dp[i] = dp[i-1] + 1
+			maxPos = i + nums[i]
+		}
+	}
+
+	return dp[n-1]
+}
