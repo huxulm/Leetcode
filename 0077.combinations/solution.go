a6ded97c868835c6a7160ac6979da5576d2c19c6
Hu Xulm <huxulm@gmail.com>
2022-02-03 12:50:40 +0800

    update 77

--- 0077.combinations/solution.go
+++ 0077.combinations/solution.go
@@ -24,21 +24,28 @@ func combine(n int, k int) [][]int {
 	return ans
 }
 
-// 0 <= idx <= k-1 第idx个数
-// prev = idx-1 第idx-1个数
-func dfs(n, k, prev, idx int, track []int, ans *[][]int) {
-	if idx == k {
-		res := make([]int, k)
-		copy(res, track)
-		*ans = append(*ans, res)
-		return
+func combine1(n int, k int) (ans [][]int) {
+	temp := []int{}
+	var dfs func(int)
+	dfs = func(cur int) {
+		// 剪枝：temp 长度加上区间 [cur, n] 的长度小于 k，不可能构造出长度为 k 的 temp
+		if len(temp)+(n-cur+1) < k {
+			return
+		}
+		// 记录合法的答案
+		if len(temp) == k {
+			comb := make([]int, k)
+			copy(comb, temp)
+			ans = append(ans, comb)
+			return
+		}
+		// 考虑选择当前位置
+		temp = append(temp, cur)
+		dfs(cur + 1)
+		temp = temp[:len(temp)-1]
+		// 考虑不选择当前位置
+		dfs(cur + 1)
 	}
-	// 选择第i个数
-	// 选择范围
-	for i := prev + 1; i <= n-k+idx; i++ {
-		// 选择
-		track[idx] = i + 1
-		// 选择idx+1个数
-		dfs(n, k, i, idx+1, track, ans)
-	}
+	dfs(1)
+	return
 }
