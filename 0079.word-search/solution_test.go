7a8b62d250a84fb2b2e7e3531ddc9b32daa342dc
Hu Xulm <huxulm@gmail.com>
2022-02-03 18:02:25 +0800

    update 79

--- 0079.word-search/solution_test.go
+++ 0079.word-search/solution_test.go
@@ -1,14 +1,25 @@
 package wordsearch
 
 import (
-	"fmt"
 	"testing"
 )
 
 func TestExists(t *testing.T) {
-	// board := [][]byte{{'A', 'B', 'C', 'E'}, {'S', 'F', 'C', 'S'}, {'A', 'D', 'E', 'E'}}
-	// word := "ASADFBCCEESE"
-	board := [][]byte{{'a'}}
-	word := "a"
-	fmt.Println(exist(board, word))
+	tests := []struct {
+		board  [][]byte
+		word   string
+		expect bool
+	}{
+		{board: [][]byte{{'A', 'B', 'C', 'E'}, {'S', 'F', 'C', 'S'}, {'A', 'D', 'E', 'E'}}, word: "DFBCCEE", expect: true},
+		{board: [][]byte{{'A', 'B', 'C', 'E'}, {'S', 'F', 'C', 'S'}, {'A', 'D', 'E', 'E'}}, word: "ASADFBCCEESE", expect: true},
+		{board: [][]byte{{'a'}}, word: "a", expect: true},
+		{board: [][]byte{{'a', 'b'}}, word: "ab", expect: true},
+		{board: [][]byte{{'a', 'b'}}, word: "abc", expect: false},
+	}
+
+	for _, test := range tests {
+		if result := exist(test.board, test.word); result != test.expect {
+			t.Errorf("Board %v word %s expect %v got %v", test.board, test.word, test.expect, result)
+		}
+	}
 }
