1f987f9fe70a12b2dbcb7e736d04adba5c1862b8
Hu Xulm <huxulm@gmail.com>
2022-02-05 12:11:51 +0800

    update 26

--- 0026.remove-duplicates-from-sorted-array/solution_test.go
+++ 0026.remove-duplicates-from-sorted-array/solution_test.go
@@ -3,5 +3,18 @@ package removeduplicatesfromsortedarray
 import "testing"
 
 func TestRemoveDuplicates(t *testing.T) {
-	removeDuplicates([]int{0, 0, 1, 1, 1, 2, 2, 3, 3, 4})
+	tests := []struct {
+		inputs []int
+		expect int
+	}{
+		{inputs: []int{1, 1}, expect: 1},
+		{inputs: []int{1, 2, 2}, expect: 2},
+		{inputs: []int{1, 2, 3, 3, 3, 4, 4, 5, 5}, expect: 5},
+	}
+
+	for _, test := range tests {
+		if result := removeDuplicates(test.inputs); test.expect != result {
+			t.Errorf("Input %v expect %d but got %d", test.inputs, test.expect, result)
+		}
+	}
 }
