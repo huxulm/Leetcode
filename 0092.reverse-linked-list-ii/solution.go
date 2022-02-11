c5bc5a081e9d7180f6f83adbb12f61d9dbd13f2c
Hu Xulm <huxulm@gmail.com>
2022-02-11 13:55:24 +0800

    add 92

--- 0092.reverse-linked-list-ii/solution.go
+++ 0092.reverse-linked-list-ii/solution.go
@@ -4,29 +4,36 @@ import (
 	. "lc/structures"
 )
 
-// { 1, 2, 3, 4, 5, 6}
-// [2,3]
-// { 1, 3, 2, 4, 5, 6}
 func reverseBetween(head *ListNode, left int, right int) *ListNode {
+
+	// 保存在开始反转处节点的前一节点(可能为空,断裂处的位置)
 	dummy := &ListNode{Next: head}
-	var reverseHead, reverseTail *ListNode = &ListNode{}, nil
 	cur := dummy
+
+	// 保存被反转部分链表的头和尾
+	var reverseHead, reverseTail *ListNode = nil, nil
+
 	for i, n := 1, head; n != nil; i++ {
 		next := n.Next
+		// cur在 i==left 时断裂
 		if i < left {
 			cur = cur.Next
 		}
+
 		if i == left {
 			reverseTail = n
 			reverseTail.Next = nil
 		}
+
+		// 反转 [left,right]
 		if i >= left && i <= right {
-			n.Next = reverseHead.Next
-			reverseHead.Next = n
+			n.Next = reverseHead
+			reverseHead = n
 		}
-		// 合并
+
+		// 合并三个部分的链表(前一部分>>反转部分>>后一部分)
 		if i == right {
-			cur.Next = reverseHead.Next
+			cur.Next = reverseHead
 			reverseTail.Next = next
 			break
 		}
