77743b910f172fee8e8827a314bf32f5c1405fce
Hu Xulm <huxulm@gmail.com>
2022-02-15 20:16:19 +0800

    update

---
+++ top-interview-questions-easy/02.string/001.reverse-string/solution.go
@@ -0,0 +1,8 @@
+package reversestring
+
+func reverseString(s []byte) {
+	n := len(s)
+	for i := 0; i < n>>1; i++ {
+		s[i], s[n-1-i] = s[n-1-i], s[i]
+	}
+}
---
+++ top-interview-questions-easy/02.string/002.reverse-integer/solution.go
@@ -0,0 +1,12 @@
+package reverseinteger
+
+func reverse(x int) (rev int) {
+	for x != 0 {
+		if rev < -1<<31/10 || rev > (1<<31-1)/10 {
+			return 0
+		}
+		rev = rev*10 + x%10
+		x /= 10
+	}
+	return
+}
