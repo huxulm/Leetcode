ce6e0998c20daab282132fb053d589717ec14b40
Hu Xulm <huxulm@gmail.com>
2022-02-19 15:53:02 +0800

    add 746

---
+++ 0746.min-cost-climbing-stairs/solution.go
@@ -0,0 +1,37 @@
+package mincostclimbingstairs
+
+func minCostClimbingStairs(cost []int) int {
+	// n >= 2
+	n := len(cost)
+	// 爬上第i层楼梯的最小花费
+	dp := make([]int, n+1)
+	dp[2] = min(cost[0], cost[1])
+
+	for i := 3; i <= n; i++ {
+		dp[i] = min(dp[i-2]+cost[i-2], dp[i-1]+cost[i-1])
+	}
+
+	return dp[n]
+}
+
+func min(x, y int) int {
+	if x < y {
+		return x
+	}
+	return y
+}
+
+// 空间降维
+// F(N) = min(F(n-2)+Cost(n-2), F(N-1)+Cost(n-1))
+func minCostClimbingStairs1(cost []int) int {
+	// n >= 2
+	n := len(cost)
+	// 爬上第i层楼梯的最小花费
+	dp1, dp2 := 0, min(cost[0], cost[1])
+
+	for i := 3; i <= n; i++ {
+		dp1, dp2 = dp2, min(dp1+cost[i-2], dp2+cost[i-1])
+	}
+
+	return dp2
+}
