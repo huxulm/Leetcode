66bf7f4f97d1572d282332f976a7331ee6eeffe8
Hu Xulm <huxulm@gmail.com>
2022-02-19 20:34:21 +0800

    update 322

--- 0322.coin-change/solution.go
+++ 0322.coin-change/solution.go
@@ -1,5 +1,6 @@
 package coinchange
 
+// 动归,自底向上
 func coinChange(coins []int, amount int) int {
 	// F(n) = 1+min{F(n-coins[0], ... , F(n-coins[i]))}  (F(n) != -1)
 	dp := make([]int, amount+1)
@@ -27,4 +28,46 @@ func min(x, y int) int {
 		return x
 	}
 	return y
+}
+
+// 动归+记忆优化搜索,自顶向下
+func coinChange1(coins []int, amount int) int {
+	memo := make([]int, amount+1)
+	// dp 数组全都初始化为特殊值
+	for i := range memo {
+		memo[i] = -666
+	}
+	return dp(coins, amount, memo)
+}
+
+func dp(coins []int, amount int, memo []int) int {
+	if amount == 0 {
+		return 0
+	}
+	if amount < 0 {
+		return -1
+	}
+	// 查备忘录，防止重复计算
+	if memo[amount] != -666 {
+		return memo[amount]
+	}
+
+	res := 1<<31 - 1
+	for _, coin := range coins {
+		// 计算子问题的结果
+		subProblem := dp(coins, amount-coin, memo)
+		// 子问题无解则跳过
+		if subProblem == -1 {
+			continue
+		}
+		// 在子问题中选择最优解，然后加一
+		res = min(res, subProblem+1)
+	}
+	// 把计算结果存入备忘录
+	if res == 1<<31-1 {
+		memo[amount] = -1
+	} else {
+		memo[amount] = res
+	}
+	return memo[amount]
 }
