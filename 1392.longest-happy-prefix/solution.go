c780464ab3a328c3ad8a0f7548fc8322590341a2
Hu Xulm <huxulm@gmail.com>
2022-03-01 16:06:45 +0800

    Update: 1392

--- 1392.longest-happy-prefix/solution.go
+++ 1392.longest-happy-prefix/solution.go
@@ -15,3 +15,20 @@ func longestPrefix(s string) (ans string) {
 	}
 	return
 }
+
+// KMP
+func longestPrefix1(s string) (ans string) {
+	n := len(s)
+	next := make([]int, n)
+	for j, i := 0, 1; i < n; i++ {
+		for j > 0 && s[j] != s[i] {
+			j = next[j-1]
+		}
+		if s[j] == s[i] {
+			j++
+		}
+		next[i] = j
+	}
+	ans = s[:next[n-1]]
+	return
+}
