f5d55a6326dd47def64a41a6ecd06469c8e6e33e
Hu Xulm <huxulm@gmail.com>
2022-03-12 14:53:37 +0800

    add 117

---
+++ 0117.populating-next-right-pointers-in-each-node-ii/solution.go
@@ -0,0 +1,55 @@
+package populatingnextrightpointersineachnodeii
+
+import (
+	. "lc/structures"
+)
+
+func connect(root *Node) *Node {
+	if root == nil {
+		return nil
+	}
+	q := []*Node{root}
+	for len(q) > 0 {
+		tmp := q
+		q = nil
+		for i, node := range tmp {
+			if i+1 < len(tmp) {
+				node.Next = tmp[i+1]
+			}
+			if node.Left != nil {
+				q = append(q, node.Left)
+			}
+			if node.Right != nil {
+				q = append(q, node.Right)
+			}
+		}
+	}
+	return root
+}
+
+func connect1(root *Node) *Node {
+	start := root
+
+	for start != nil {
+		var nextStart, last *Node
+		handle := func(cur *Node) {
+			if cur == nil {
+				return
+			}
+			if nextStart == nil {
+				nextStart = cur
+			}
+			if last != nil {
+				last.Next = cur
+			}
+			last = cur
+		}
+		for p := start; p != nil; p = p.Next {
+			handle(p.Left)
+			handle(p.Right)
+		}
+		start = nextStart
+	}
+
+	return root
+}
