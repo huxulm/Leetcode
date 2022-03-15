6fa8445682b489d348dfdc6a3e5a4d74311afc71
Hu Xulm <huxulm@gmail.com>
2022-03-15 16:35:53 +0800

    update 40

--- 0040.combination-sum-ii/solution.go
+++ 0040.combination-sum-ii/solution.go
@@ -19,6 +19,7 @@ func combinationSum2(candidates []int, target int) (res [][]int) {
 			return
 		}
 		for i := idx; i < n; i++ {
+			// 要对同一树层使用过的元素进行跳过
 			if i > idx && candidates[i] == candidates[i-1] {
 				continue
 			}
@@ -32,5 +33,38 @@ func combinationSum2(candidates []int, target int) (res [][]int) {
 
 	backtrack(target, 0)
 
+	return
+}
+
+func combinationSum2_1(candidates []int, target int) (ans [][]int) {
+	sort.Ints(candidates)
+	n := len(candidates)
+	track := []int{}
+	var backtrack func(target, sum, idx int)
+	backtrack = func(target, sum, idx int) {
+		if sum == target {
+			ans = append(ans, append([]int(nil), track...))
+			return
+		}
+
+		if idx == n {
+			return
+		}
+
+		for i := idx; i < n; i++ {
+			// 要对同一树层使用过的元素进行跳过
+			if i > idx && candidates[i] == candidates[i-1] {
+				continue
+			}
+			if target >= candidates[i]+sum {
+				track = append(track, candidates[i])
+				sum += candidates[i]
+				backtrack(target, sum, i+1)
+				sum -= candidates[i]
+				track = track[:len(track)-1]
+			}
+		}
+	}
+	backtrack(target, 0, 0)
 	return
 }
