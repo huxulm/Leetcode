3fbb383299ea62d6fad21775ae1dddb6db532c46
Hu Xulm <huxulm@gmail.com>
2022-03-18 12:15:01 +0800

    rebuild

---
+++ 0301-0350/0343.integer-break/solution.go
@@ -0,0 +1,34 @@
+package integerbreak
+
+import "math"
+
+// 方法一: 答案对 但 方法不正确
+func integerBreak(n int) (ans int) {
+	dp := n / 2 * (n/2 + n%2)
+	ans = dp
+	for i := 2; i < n; i++ {
+		dp = max(int(math.Pow(float64(n/i), float64(i-1)))*(n/i+n%i), int(math.Pow(float64(n/i+1), float64(i-1)))*(n-(i-1)*(n/i+1)))
+		if dp > ans {
+			ans = dp
+		}
+	}
+
+	return
+}
+
+func max(x, y int) int {
+	if x > y {
+		return x
+	}
+	return y
+}
+
+// 动态规划
+func integerBreak1(n int) (ans int) {
+	// dp[i] n 拆分成 i 个整数乘积的最大值
+	dp := make([]int, n+1)
+
+	dp[2] = n / 2 * (n/2 + n%2)
+
+	return
+}
