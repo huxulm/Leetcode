65e835eff0865d4899ff8c7aa012b35208632a75
Hu Xulm <huxulm@gmail.com>
2022-04-19 10:48:32 +0800

    update 386

--- 0351-0400/0386.lexicographical-numbers/solution.go
+++ 0351-0400/0386.lexicographical-numbers/solution.go
@@ -21,3 +21,21 @@ func lexicalOrder(n int) (ans []int) {
 	}
 	return
 }
+
+func lexicalOrder1(n int) (ans []int) {
+	ans = make([]int, n)
+
+	num := 1
+	for i := range ans {
+		ans[i] = num
+		if num*10 <= n {
+			num *= 10
+		} else {
+			for num%10 == 9 || num+1 > n {
+				num /= 10
+			}
+			num++
+		}
+	}
+	return
+}
