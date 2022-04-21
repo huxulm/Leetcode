c7b77991c94b3f06d85c1de4cb91d1a90e34d9b2
Hu Xulm <huxulm@gmail.com>
2022-04-21 11:03:42 +0800

    update 824

--- 0801-0850/0824.goat-latin/soluiton.go
+++ 0801-0850/0824.goat-latin/soluiton.go
@@ -23,3 +23,34 @@ func toGoatLatin(sentence string) string {
 	}
 	return ans.String()
 }
+
+func toGoatLatin1(s string) string {
+	n := len(s)
+	last, cnt := 0, 0
+	ans := &strings.Builder{}
+	for i := 0; i < n; i++ {
+
+		for i < n && s[i] != ' ' {
+			i++
+		}
+		cnt++
+		w := s[last:i]
+		last = i + 1
+		if strings.IndexByte("aeiou", byte(w[0]|' ')) != -1 {
+			ans.WriteString(w)
+		} else {
+			ans.WriteString(w[1:])
+			ans.WriteString(w[:1])
+		}
+		ans.WriteString("ma")
+
+		for j := 0; j <= cnt; j++ {
+			ans.WriteString("a")
+		}
+		if i != n {
+			ans.WriteString(" ")
+		}
+		last = i - 1
+	}
+	return ans.String()
+}
