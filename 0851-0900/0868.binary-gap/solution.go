519b3c2409e901532b66f4768296a323be49a9f8
Hu Xulm <huxulm@gmail.com>
2022-04-24 10:01:02 +0800

    add 868

---
+++ 0851-0900/0868.binary-gap/solution.go
@@ -0,0 +1,24 @@
+package binarygap
+
+func binaryGap(n int) (ans int) {
+	last := -1
+
+	for i := 0; n > 0; i, n = i+1, n>>1 {
+		if n&1 == 1 {
+			if last == -1 {
+				last = i
+			} else {
+				ans = max(i-last, ans)
+				last = i
+			}
+		}
+	}
+	return
+}
+
+func max(x, y int) int {
+	if x > y {
+		return x
+	}
+	return y
+}
