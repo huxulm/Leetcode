e07cfdf798fc0507304ab75cc64cdd0a6911bd90
Hu Xulm <huxulm@gmail.com>
2022-05-09 12:35:02 +0800

    weekly-292 6058

---
+++ 6051-6100/6058.count-number-of-texts/solution_test.go
@@ -0,0 +1,14 @@
+package countnumberoftexts
+
+import (
+	"testing"
+
+	"github.com/EndlessCheng/codeforces-go/leetcode/testutil"
+)
+
+func TestXxx(t *testing.T) {
+	targetCaseNum := 0 // -1
+	if err := testutil.RunLeetCodeFuncWithFile(t, countTexts, "./in.txt", targetCaseNum); err != nil {
+		t.Fatal(err)
+	}
+}
