2475eeb3f9432fadcfc0408a727cb5017b29e0bd
Hu Xulm <huxulm@gmail.com>
2022-05-12 10:12:21 +0800

    Update 944

--- 0901-0950/0944.delete-columns-to-make-sorted/solution.go
+++ 0901-0950/0944.delete-columns-to-make-sorted/solution.go
@@ -2,18 +2,12 @@ package deletecolumnstomakesorted
 
 func minDeletionSize(strs []string) (ans int) {
 	m, n := len(strs), len(strs[0])
-
 	for j := 0; j < n; j++ {
-		cnt := 1
 		for i := 0; i < m-1; i++ {
-			if strs[i][j] <= strs[i+1][j] {
-				cnt++
-				continue
+			if strs[i][j] > strs[i+1][j] {
+				ans++
+				break
 			}
-			break
-		}
-		if cnt != m {
-			ans++
 		}
 	}
 	return
