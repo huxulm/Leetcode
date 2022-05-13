d4f894a99e09da80be8c4a74737c695aecadb6e7
Hu Xulm <huxulm@gmail.com>
2022-05-13 19:43:19 +0800

    update 1760

--- 1751-1800/1760.minimum-limit-of-balls-in-a-bag/solution.go
+++ 1751-1800/1760.minimum-limit-of-balls-in-a-bag/solution.go
@@ -8,7 +8,10 @@ func minimumSize(nums []int, maxOps int) int {
 		i++
 		tot := 0
 		for _, v := range nums {
-			// 向下取整
+			// 两种情况:
+			// 1. v 能整除 i, 操作次数 = v/i - 1
+			// 2. v 不能整除 i, 操作次数 = v/i
+			// 合并为: 向下取整
 			tot += (v - 1) / i
 		}
 		return tot <= maxOps
