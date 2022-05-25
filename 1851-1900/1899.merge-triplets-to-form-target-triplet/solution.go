c75f4edb20864f96201f9087c600f39ca8c04b88
Hu Xulm <huxulm@gmail.com>
2022-05-25 11:02:44 +0800

    update

--- 1851-1900/1899.merge-triplets-to-form-target-triplet/solution.go
+++ 1851-1900/1899.merge-triplets-to-form-target-triplet/solution.go
@@ -1,42 +1,23 @@
 package mergetripletstoformtargettriplet
 
-import (
-	"sort"
-)
-
 func mergeTriplets(triplets [][]int, target []int) bool {
 	ta, tb, tc := target[0], target[1], target[2]
-
-	tmp := [][]int{}
+	var hasA, hasB, hasC bool
 	for _, t := range triplets {
+		if hasA && hasB && hasC {
+			break
+		}
 		if t[0] <= ta && t[1] <= tb && t[2] <= tc {
-			tmp = append(tmp, t)
+			if t[0] == ta {
+				hasA = true
+			}
+			if t[1] == tb {
+				hasB = true
+			}
+			if t[2] == tc {
+				hasC = true
+			}
 		}
 	}
-
-	triplets = tmp
-	n := len(triplets)
-	if n == 0 {
-		return false
-	}
-
-	sort.Slice(triplets, func(i, j int) bool { return triplets[i][0] < triplets[j][0] })
-
-	if triplets[n-1][0] != ta {
-		return false
-	}
-
-	sort.Slice(triplets, func(i, j int) bool { return triplets[i][1] < triplets[j][1] })
-
-	if triplets[n-1][1] != tb {
-		return false
-	}
-
-	sort.Slice(triplets, func(i, j int) bool { return triplets[i][2] < triplets[j][2] })
-
-	if triplets[n-1][2] != tc {
-		return false
-	}
-
-	return true
+	return hasA && hasB && hasC
 }
