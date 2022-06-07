1328f4eccbe772f77286fd9f3352e4c564338663
Hu Xulm <huxulm@gmail.com>
2022-06-07 10:44:15 +0800

    update

--- 0851-0900/0875.koko-eating-bananas/solution.go
+++ 0851-0900/0875.koko-eating-bananas/solution.go
@@ -3,14 +3,11 @@ package kokoeatingbananas
 import "sort"
 
 func minEatingSpeed(piles []int, h int) int {
-
-	// f(x) 先 true 后 false，返回最小的
-	// 使得 f(x) 为 true 的答案
 	return sort.Search(1e9, func(k int) bool {
 		// k++
 		tot := 0
 		for i := range piles {
-			// 向上取整 a+b-1/b
+			// 向上取整 (a+b-1)/b
 			tot += (piles[i] + k) / (k + 1)
 		}
 		return tot <= h
@@ -40,6 +37,46 @@ func minEatingSpeed1(piles []int, h int) int {
 			l = mid + 1
 		} else {
 			r = mid
+		}
+	}
+	return l
+}
+
+// 2022/06/07
+// 二分
+func minEatingSpeed2(piles []int, h int) int {
+	return sort.Search(1e9, func(s int) bool {
+		s++
+		tot := 0
+		for _, v := range piles {
+			tot += (v + s - 1) / s
+		}
+		return tot <= h
+	})
+}
+
+func minEatingSpeed3(piles []int, h int) int {
+	var check = func(s int) bool {
+		tot := 0
+		for i := range piles {
+			tot += (piles[i] + s - 1) / s
+		}
+		return tot <= h
+	}
+	l, r := 1, -1
+
+	for i := range piles {
+		if piles[i] > r {
+			r = piles[i]
+		}
+	}
+
+	for l < r {
+		mid := l + (r-l)>>1
+		if check(mid) {
+			r = mid
+		} else {
+			l = mid + 1
 		}
 	}
 	return l
